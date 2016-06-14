fe12eba3d74ed2a90c62d3d1f184dabefaa1a0f4
Joe Sherratt <Joey@Joes-MacPro.local>
2016-06-15 00:59:33 +0100

    Works on all device sizes. Custom data model for trivia. Four answers for each question and random questions. Displays correct answer if player answers incorrectly

--- TrueFalseStarter/TriviaModel.swift
+++ TrueFalseStarter/TriviaModel.swift
@@ -9,19 +9,44 @@
 import Foundation
 import GameKit
 
-struct Trivia {
+class Trivia {
     
-    let triviaQuestions: [[String : String]] = [
-        ["Question": "Only female koalas can whistle", "Answer": "False"],
-        ["Question": "Blue whales are technically whales", "Answer": "True"],
-        ["Question": "Camels are cannibalistic", "Answer": "False"],
-        ["Question": "All ducks are birds", "Answer": "True"]
-    ]
+    var question: String
+    var option1:String
+    var option2:String
+    var option3:String
+    var option4:String
+    var answer:String
     
-    func getRandomTrivia() -> [String:String] {
+    init(question: String, option1: String, option2: String, option3: String, option4: String, answer: String) {
         
-        let randomIndex = GKRandomSource.sharedRandom().nextIntWithUpperBound(triviaQuestions.count)
-        
-        return triviaQuestions[randomIndex]
+        self.question = question
+        self.option1 = option1
+        self.option2 = option2
+        self.option3 = option3
+        self.option4 = option4
+        self.answer = answer
     }
 }
+
+let question1 = Trivia(question: "What is the closest planet to the Sun?", option1: "Venus", option2: "Mercury", option3: "Earth", option4: "Mars", answer: "Mercury")
+
+let question2 = Trivia(question: "What planet is known as the red planet?", option1: "Mars", option2: "Jupiter", option3: "Neptune", option4: "Saturn", answer: "Mars")
+
+let question3 = Trivia(question: "Earth is located in which galaxy?", option1: "Andromeda", option2: "Tadpole", option3: "Pinwheel", option4: "The Milky Way", answer: "The Milky Way")
+
+let question4 = Trivia(question: "What is the name of the first satellite sent into space?", option1: "Quazer", option2: "Sputnik", option3: "Atlas", option4: "Polaris", answer: "Sputnik")
+
+let question5 = Trivia(question: "Who was the first person to walk on the moon?", option1: "Neil Armstrong", option2: "Michael Collins", option3: "Buzz Aldrin", option4: "Pete Conrad", answer: "Neil Armstrong")
+
+let question6 = Trivia(question: "What is the name of the force holding us to the Earth?", option1: "Interstellar", option2: "Fusion", option3: "Ironis", option4: "Gravity", answer: "Gravity")
+
+let question7 = Trivia(question: "Ganymede is a moon of which planet?", option1: "Neptune", option2: "Saturn", option3: "Jupiter", option4: "Venus", answer: "Jupiter")
+
+let question8 = Trivia(question: "What is the name of NASA’s most famous space telescope?", option1: "Hubble", option2: "Spitzer", option3: "James Webb", option4: "Giant Magellan", answer: "Hubble")
+
+let question9 = Trivia(question: "What planet is famous for the beautiful rings that surround it?", option1: "Pluto", option2: "Venus", option3: "Saturn", option4: "Uranus", answer: "Saturn")
+
+let question10 = Trivia(question: "What is the sun?", option1: "Planet", option2: "Star", option3: "Atom", option4: "Supernova", answer: "Star")
+
+var questionArray = [question1,question2,question3,question4,question5,question6,question7,question8,question9,question10]
--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -12,23 +12,33 @@ import AudioToolbox
 
 class ViewController: UIViewController {
     
-    let triviaModel = Trivia()
-    
     let questionsPerRound = 4
     var questionsAsked = 0
     var correctQuestions = 0
     var indexOfSelectedQuestion: Int = 0
+    var shuffledArray: [Trivia] = []
+    var btnArray: [UIButton] = []
     
     var gameSound: SystemSoundID = 0
     
     @IBOutlet weak var questionField: UILabel!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var option1Btn: UIButton!
+    @IBOutlet weak var option2Btn: UIButton!
+    @IBOutlet weak var option3Btn: UIButton!
+    @IBOutlet weak var option4Btn: UIButton!
+    @IBOutlet weak var nextQuestionBtn: UIButton!
     @IBOutlet weak var playAgainButton: UIButton!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        btnArray.append(option1Btn)
+        btnArray.append(option2Btn)
+        btnArray.append(option3Btn)
+        btnArray.append(option4Btn)
+        
         //loadGameStartSound()
         // Start game
         playGameStartSound()
@@ -42,54 +52,103 @@ class ViewController: UIViewController {
     
     func displayQuestion() {
         
-        let question = triviaModel.getRandomTrivia()
-        questionField.text = question["Question"]
+        shuffledArray =  GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(questionArray) as! [Trivia]
+        
+        indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextIntWithUpperBound(shuffledArray.count)
+        
+        let selectedQuestion = shuffledArray[indexOfSelectedQuestion]
+        questionField.text = selectedQuestion.question
+        
+        option1Btn.setTitle(selectedQuestion.option1, forState: .Normal)
+        option2Btn.setTitle(selectedQuestion.option2, forState: .Normal)
+        option3Btn.setTitle(selectedQuestion.option3, forState: .Normal)
+        option4Btn.setTitle(selectedQuestion.option4, forState: .Normal)
+        
+        answerLabel.text = ""
         playAgainButton.hidden = true
+        nextQuestionBtn.hidden = false
+        nextQuestionBtn.enabled = false
     }
     
     func displayScore() {
         // Hide the answer buttons
-        trueButton.hidden = true
-        falseButton.hidden = true
+        option1Btn.hidden = true
+        option2Btn.hidden = true
+        option3Btn.hidden = true
+        option4Btn.hidden = true
         
         // Display play again button
         playAgainButton.hidden = false
+        nextQuestionBtn.hidden = true
         
-        questionField.text = "Way to go!\nYou got \(correctQuestions) out of \(questionsPerRound) correct!"
-        
+        if correctQuestions == questionsAsked {
+            
+            questionField.text = "Amazing!\nYou got \(correctQuestions) out of \(questionsPerRound) correct!"
+            
+        }else if correctQuestions > 1 && correctQuestions < 4 {
+            
+            questionField.text = "Way to go!\nYou got \(correctQuestions) out of \(questionsPerRound) correct!"
+            
+        }else {
+            
+            questionField.text = "Brush up on your knowledge!\nYou got \(correctQuestions) out of \(questionsPerRound) correct!"
+        }
     }
     
     @IBAction func checkAnswer(sender: UIButton) {
+        
         // Increment the questions asked counter
         questionsAsked += 1
         
-        let selectedQuestionDict = triviaModel.getRandomTrivia()
-        let correctAnswer = selectedQuestionDict["Answer"]
+        disableBtns(sender.tag)
+        nextQuestionBtn.enabled = true
         
-        if (sender === trueButton &&  correctAnswer == "True") || (sender === falseButton && correctAnswer == "False") {
+        let selectedQuestion = shuffledArray[indexOfSelectedQuestion]
+        let correctAnswer = selectedQuestion.answer
+        
+        if sender.currentTitle == correctAnswer {
+            
             correctQuestions += 1
-            questionField.text = "Correct!"
-        } else {
-            questionField.text = "Sorry, wrong answer!"
+            
+            sender.enabled = true
+            sender.titleLabel?.textColor = UIColor.whiteColor()
+            
+            answerLabel.textColor = UIColor(colorLiteralRed: 82/255, green: 223/255, blue: 107/255, alpha: 1.0)
+            answerLabel.text = "Correct!"
+            
+        }else {
+            
+            answerLabel.textColor = UIColor(colorLiteralRed: 218/255, green: 75/255, blue: 75/255, alpha: 1.0)
+            answerLabel.text = "Sorry\nthe correct answer is \(correctAnswer)"
         }
-        
-        loadNextRoundWithDelay(seconds: 2)
-    }
+}
     
     func nextRound() {
         if questionsAsked == questionsPerRound {
+            
             // Game is over
+            answerLabel.text = ""
             displayScore()
+            
         } else {
+            
             // Continue game
             displayQuestion()
+            enableBtns()
         }
+    }
+    @IBAction func nextQuestion(sender: UIButton) {
+        
+        loadNextRoundWithDelay(seconds: 1)
     }
     
     @IBAction func playAgain() {
+        
         // Show the answer buttons
-        trueButton.hidden = false
-        falseButton.hidden = false
+        option1Btn.hidden = false
+        option2Btn.hidden = false
+        option3Btn.hidden = false
+        option4Btn.hidden = false
         
         questionsAsked = 0
         correctQuestions = 0
@@ -99,6 +158,31 @@ class ViewController: UIViewController {
 
     
     // MARK: Helper Methods
+    
+    func disableBtns(tag: Int) {
+        
+        for btn in btnArray {
+            
+            if btn.tag == tag {
+                
+                btn.enabled = true
+                btn.userInteractionEnabled = false
+                
+            }else {
+                
+                btn.enabled = false
+            }
+        }
+    }
+    
+    func enableBtns() {
+        
+        for btn in btnArray {
+            
+            btn.enabled = true
+            btn.userInteractionEnabled = true
+        }
+    }
     
     func loadNextRoundWithDelay(seconds seconds: Int) {
         // Converts a delay in seconds to nanoseconds as signed 64 bit integer
